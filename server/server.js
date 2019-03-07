9f4c1da6c4a4eb0b1d5a245e6276a0495a9bb052
boonHanalytics2018 <boonsiang.teh@biomind.ai>
2019-03-07 17:09:22 +0800

    Added get route for /todos

--- server/server.js
+++ server/server.js
@@ -7,8 +7,10 @@ const {User} = require('./models/user');
 
 var app = express();
 
+// use bodyParser middleware to
 app.use(bodyParser.json());
 
+// create a /todos route for post requests to create todos in our Todo collection in Mongodb
 app.post("/todos", (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -20,6 +22,16 @@ app.post("/todos", (req, res) => {
     res.status(400).send(error);
   });
 });
+
+// get request for /todos route to get all todos
+app.get('/todos', (req, res) => {
+  // get all documents in Todo collection
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.send(err);
+  })
+})
 
 app.listen(3005, () => {
   console.log("Started on port 3005");
